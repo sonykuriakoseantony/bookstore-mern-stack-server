6ebff303445f26e81ea6eb5e3fb5f890d0a19d05
sonykuriakoseantony <sonykuriakoseantony@gmail.com>
2025-12-17 15:34:21 +0530

    all books except for logged in user, all user books, all user bought books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -8,21 +8,21 @@ exports.addBookController = async (req, res) => {
     const sellerMail = req.payload; //retrieved from jwtMiddleware
     const uploadImg = req.files.map(file => file.filename);
     console.log(title, author, pages, imageURL, price, discountPrice, abstract, publisher, language, isbn, category, uploadImg, sellerMail);
-    try{
+    try {
         console.log("Check All 3 images uploaded");
         console.log(uploadImg);
-        const existingBook = await books.findOne({title, sellerMail})
-        if(existingBook){
+        const existingBook = await books.findOne({ title, sellerMail })
+        if (existingBook) {
             res.status(409).json("Book already exists! Upload a different book");
         }
-        else{
+        else {
             const newBook = await books.create({
                 title, author, pages, imageURL, price, discountPrice, abstract, publisher, language, isbn, category, uploadImg, sellerMail
             })
             res.status(200).json(newBook);
         }
     }
-    catch(error){
+    catch (error) {
         console.log(error);
         res.status(500).json(error);
     }
@@ -31,17 +31,59 @@ exports.addBookController = async (req, res) => {
 // get home page books - latest 4 books - Authorized users only
 exports.getHomeBookController = async (req, res) => {
     console.log("Inside get home books Controller");
-    try{
-        const homeBooks = await books.find().sort({_id : -1}).limit(4);
+    try {
+        const homeBooks = await books.find().sort({ _id: -1 }).limit(4);
         res.status(200).json(homeBooks)
     }
-    catch(error){
+    catch (error) {
         console.log(error);
         res.status(500).json(error);
     }
 }
 
-//
+// get all books except loggedin user - Authorized users only
+exports.getUserAllBooksController = async (req, res) => {
+    console.log("Inside getUserAllBooksController");
+    const loginUserMail = req.payload;
 
-//
+    try {
+        const allBooks = await books.find({ sellerMail: { $ne: loginUserMail } });
+        res.status(200).json(allBooks)
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+}
+
+
+// get all books uploaded by user - Authorized users only
+exports.getUserProfileBooksController = async (req, res) => {
+    console.log("Inside getUserProfileBooksController");
+    const loginUserMail = req.payload;
+
+    try {
+        const userBooks = await books.find({ sellerMail: { $eq: loginUserMail } });
+        res.status(200).json(userBooks)
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+}
+
+// get user bought books - Authorized users only
+exports.getUserBoughtBooksController = async (req, res) => {
+    console.log("Inside getUserBoughtBooksController");
+    const loginUserMail = req.payload;
+
+    try {
+        const userBoughtBooks = await books.find({ buyerMail : loginUserMail });
+        res.status(200).json(userBoughtBooks)
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+}
 
--- routes/router.js
+++ routes/router.js
@@ -15,12 +15,22 @@ router.post('/login', userController.loginController);
 //google login routes here
 router.post('/google-login', userController.googleLoginController);
 
-/**----------------------------Authorised Users only-------------------------**/
 // home books
 router.get('/home/books', bookController.getHomeBookController);
 
+/**----------------------------Authorised Users only-------------------------**/
+
 //add books 
 router.post('/user/add/book', jwtMiddleware, multerMiddleware.array('uploadImg', 3), bookController.addBookController)
+
+// all books except for logged in user
+router.get('/all-books', jwtMiddleware, bookController.getUserAllBooksController);
+
+// all user books
+router.get('/user-books', jwtMiddleware, bookController.getUserProfileBooksController);
+
+// all user bought books
+router.get('/user-books/bought', jwtMiddleware, bookController.getUserBoughtBooksController);
 
 module.exports = router
 
